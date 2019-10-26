33edc1cb5b1c9a510e3d639343724d8550893795
Patrick Gingras <775.pg.12@gmail.com>
2019-10-26 11:12:18 -0400

    added store for putting global state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
 import styles from "./css/App.module.scss";
 
+import StoreProvider from "./store";
+
 function App() {
   return (
-    <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles.appLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StoreProvider>
+      <div className={styles.app}>
+        <header className={styles.appHeader}>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className={styles.appLink}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </StoreProvider>
   );
 }
 
